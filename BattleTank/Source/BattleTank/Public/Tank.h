d4767fd622b39d06d94a2c0f5f9736e65339d880
Ricky <guagin0972@gmail.com>
2019-01-05 15:45:31 +0800

    using forward declaration .
    make sure that header file would only include unreal class.

--- BattleTank/Source/BattleTank/Public/Tank.h
+++ BattleTank/Source/BattleTank/Public/Tank.h
@@ -4,10 +4,11 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/Pawn.h"
-#include "TankAimingComponent.h"
 #include "Tank.generated.h"
 
-class UTankBarrel; // Forward Delaration.
+// Forward Delaration.
+class UTankBarrel; 
+class UTankAimingComponent;
 
 UCLASS()
 class BATTLETANK_API ATank : public APawn
