257ddcf17a49bf3ee6e80f5f8badedc22e46cf37
Ricky <guagin0972@gmail.com>
2019-01-22 22:26:11 +0800

    refactor the blueprint spring wheel to C++ one.

---
+++ BattleTank/Source/BattleTank/Private/SprungWheel.cpp
@@ -0,0 +1,37 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "SprungWheel.h"
+#include "PhysicsEngine/PhysicsConstraintComponent.h"
+#include "Components/StaticMeshComponent.h"
+
+// Sets default values
+ASprungWheel::ASprungWheel()
+{
+
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+	// to create the same hierarchy to them blueprint one.
+	Mass = CreateDefaultSubobject<UStaticMeshComponent>(FName("Mass"));
+	SetRootComponent(Mass);
+
+	Srping = CreateDefaultSubobject<UPhysicsConstraintComponent>(FName("Srping"));
+	Srping->SetupAttachment(Mass);
+
+	Wheel = CreateDefaultSubobject<UStaticMeshComponent>(FName("Wheel"));
+	Wheel->SetupAttachment(Mass);
+}
+
+// Called when the game starts or when spawned
+void ASprungWheel::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ASprungWheel::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
